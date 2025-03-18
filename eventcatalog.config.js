95c8755d2a4fe33010544ce97789f4a6c0b44fa0
SumitIntraedge <smanohar@partner.fluidra.com>
2025-03-18 21:09:55 +0530

    code added

---
+++ eventcatalog.config.js
@@ -0,0 +1,43 @@
+import path from 'path';
+import url from 'url';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+/** @type {import('@eventcatalog/core/bin/eventcatalog.config').Config} */
+export default {
+  cId: 'a53515c7-ae6f-4978-ac44-c26af1263f1d',
+  title: 'EventBridge POC',
+  tagline: 'Event-driven architecture with AWS EventBridge',
+  organizationName: 'OurLogix',
+  homepageLink: 'https://eventcatalog.dev/',
+  landingPage: '',
+  editUrl: 'https://github.com/boyney123/eventcatalog-demo/edit/master',
+  // By default set to false, add true to get urls ending in /
+  trailingSlash: false,
+  // Change to make the base url of the site different, by default https://{website}.com/docs,
+  // changing to /company would be https://{website}.com/company/docs,
+  base: '/',
+  // Customize the logo, add your logo to public/ folder
+  logo: {
+    alt: 'EventCatalog Logo',
+    src: '/logo.png',
+    text: 'OurLogix',
+  },
+  docs: {
+    sidebar: {
+      // Should the sub heading be rendered in the docs sidebar?
+      showPageHeadings: true,
+    },
+  },
+  generators: [
+    [
+      '@eventcatalog/generator-eventbridge',
+      {
+        licenseKey: '5WIW-ILKT-B8NL-5SWW-7LFT-798I',
+        region: 'us-east-2',
+        registryName: 'my-schema-registry',
+     
+      },
+    ],
+  ],
+};
