b91fc5e33b0663a9d9dd8527a2820506f19b7629
SumitIntraedge <smanohar@partner.fluidra.com>
2025-03-24 14:17:56 +0530

    code added

--- docs/content-modules.mjs
+++ docs/content-modules.mjs
@@ -1,6 +1,6 @@
 
 export default new Map([
 ["../domains/eventbridge/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=..%2Fdomains%2Feventbridge%2Findex.mdx&astroContentModuleFlag=true")],
-["../domains/eventbridge/services/Publisher Service/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=..%2Fdomains%2Feventbridge%2Fservices%2FPublisher+Service%2Findex.mdx&astroContentModuleFlag=true")],
-["../domains/eventbridge/services/Consumer Service/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=..%2Fdomains%2Feventbridge%2Fservices%2FConsumer+Service%2Findex.mdx&astroContentModuleFlag=true")]]);
+["../domains/eventbridge/services/Consumer Service/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=..%2Fdomains%2Feventbridge%2Fservices%2FConsumer+Service%2Findex.mdx&astroContentModuleFlag=true")],
+["../domains/eventbridge/services/Publisher Service/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=..%2Fdomains%2Feventbridge%2Fservices%2FPublisher+Service%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
